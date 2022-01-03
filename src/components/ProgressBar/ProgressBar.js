2dd52a4b77a1fbc122823c5a87aa3d5b7720eddb
Ian Barnett <ianbarnett8@gmail.com>
2022-01-04 00:17:51 +1100

    Added ProgressBar size support

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,35 +5,62 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const MeterWrapper = styled.div`
+const MeterBase = styled.div`
    width: 370px;
-   height: 24px;
-   border-radius: 8px;
+   height: 12px;
+   border-radius: 4px;
    background: hsla(0, 0%, 50%, 0.15);
    box-shadow: 0px 2px 4px 0px hsla(0, 0%, 50%, 0.35) inset;
    position: relative;
-   &:after {
-     top: 30px;
-     position: absolute;
-   }
 `;
+
+const LargeMeter = styled(MeterBase)`
+   height: 24px;
+   border-radius: 8px;
+`;
+const MediumMeter = styled(MeterBase)`
+   height: 12px;
+   border-radius: 4px;
+`;
+const SmallMeter = styled(MeterBase)`
+   height: 8px;
+   border-radius: 4px;
+`;
+
+const meters = {
+  small: SmallMeter,
+  medium: MediumMeter,
+  large: LargeMeter
+};
 
 const Progress = styled.div`
   position: absolute;
-  width: ${p => (362 * p.value / 100)}px;
-  height: 16px;
+  ${SmallMeter} & {
+    height: 8px;
+    width: ${p => (370 * (p.value / 100))}px;
+  }
+  ${MediumMeter} & {
+    height: 12px;
+    width: ${p => (370 * (p.value / 100))}px;
+  }
+  ${LargeMeter} & {
+    height: 16px;
+    top: 4px;
+    left: 4px;
+    width: ${p => (362 * (p.value / 100))}px;
+  }
   background: #4747EB;
   border-radius: 4px ${p=>(p.value>=99 ? 4*(p.value-99) : 0)}px ${p=>p.value>=99 ? 4*(p.value-99) : 0}px 4px;
   background: hsla(240, 80%, 60%, 1);
-  top: 4px;
-  left: 4px;
 `;
 
 const ProgressBar = ({ value, size }) => {
   const adjustedValue = Math.min(100, Math.max(0, value));
-  return <MeterWrapper role="progressbar" aria-valuenow={adjustedValue} aria-valuemin="0" aria-valuemax="100">
+  const Meter = meters[size] || MediumMeter;
+  
+  return <Meter size={size} role="progressbar" aria-valuenow={adjustedValue} aria-valuemin="0" aria-valuemax="100">
     <Progress value={adjustedValue} />
-  </MeterWrapper>;
+  </Meter>;
 };
 
 export default ProgressBar;
